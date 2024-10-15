8bb033b59e3cffe0e0c7829a499e0b2f60dc6640
AndreyGOIT <ae.borga@gmail.com>
2024-10-15 09:38:48 +0300

    add Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+  return (
+    <footer className="bg-midnight-navy text-frost-white p-4 absolute bottom-0 w-full">
+      <div className="container mx-auto text-center">
+        <p>© ABC Planning</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
